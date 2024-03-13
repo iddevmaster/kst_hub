d06eaa73eee5bb3a0bb86540479f5945ae4007e4
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-03-13 10:06:53 +0700

    Update QuizController.php

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -325,17 +325,6 @@ class QuizController extends Controller
             }
         }
 
-        // Activitylog::create([
-        //     'user' => auth()->id(),
-        //     'module' => 'Quiz',
-        //     'content' => $quiz->id,
-        //     'note' => 'copy',
-        // ]);
-        // Log::channel('activity')->info('User '. $request->user()->name .' copy quiz',
-        // [
-        //     'user' => $request->user(),
-        //     'quiz' => $quiz,
-        // ]);
         return response()->json(['success' => $copyQuiz]);
     }
 }
