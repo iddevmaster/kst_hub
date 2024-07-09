411de38c1e503b25640e27be7d9c8a855fb0ceb3
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-07-09 10:32:30 +0700

    Update allquizzes.blade.php

--- resources/views/page/quizzes/allquizzes.blade.php
+++ resources/views/page/quizzes/allquizzes.blade.php
@@ -124,7 +124,7 @@
                             @foreach ($quizs as $index => $quiz)
                                 <tr class="bg-white border-b  hover:bg-gray-50 ">
                                     <th class="px-6 py-4">
-                                        {{ $index+1 }}
+                                        {{ (($quizs->currentpage() -1) * 10) + $index+1 }}
                                     </th>
                                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
                                         {{ $quiz->title }}
