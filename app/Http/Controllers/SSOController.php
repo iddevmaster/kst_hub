d3b7c3ab7831483e35f986fa3fe790ce6445ff25
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-07-17 15:31:55 +0700

    Update SSOController.php

--- app/Http/Controllers/SSOController.php
+++ app/Http/Controllers/SSOController.php
@@ -64,92 +64,97 @@ class SSOController extends Controller
             } catch (\Throwable $th) {
                 return redirect('login')->withError("Failed to get login information! Try again.");
             }
-            $user = User::where('username', $username)->first();
-            if (!$user) {
-                $user = new User;
-                $user->name = $userArray['name'];
-                $user->username = $username;
-                $user->password = $username;
-
-                $agn = agency::where('agn_id', $userArray['agency']['agn_id'])->first();
-                if ($agn) {
-                    $user->agency = $agn->id;
-                } else {
-                    $agn = agency::create(['name' => $userArray['agency']['name'], 'agn_id' => $userArray['agency']['agn_id']]);
-                    $user->agency = $agn->id;
-                }
-                $brn = branch::where('brn_id', $userArray['branch']['brn_id'])->first();
-                if ($brn) {
-                    $user->brn = $brn->id;
-                } else {
-                    $brn = new branch;
-                    $brn->name = $userArray['branch']['name'];
-                    $brn->brn_id = $userArray['branch']['brn_id'];
-                    $brn->agency = $agn->id;
-                    $brn->save();
-                    $user->brn = $brn->id;
-                }
-
-                $user->dpm = '';
-
-                $user->role = $userArray['role'];
-                if (!Role::where('name', $userArray['role'])->exists()) {
-                    Role::create(['name' => $userArray['role']]);
-                }
-                if (!$user->hasRole($userArray['role'])) {
-                    $user->assignRole($userArray['role']);
-                }
+            if ($userArray['role'] == 'staff') {
+                $user = User::where('username', 'iddriverstaff')->first();
             } else {
-                $agn = agency::where('agn_id', $userArray['agency']['agn_id'])->orWhere('name', $userArray['agency']['name'])->first();
-                if ($agn) {
-                    $agn->update(['name' => $userArray['agency']['name'], 'agn_id' => $userArray['agency']['agn_id']]);
-                    $user->agency = $agn->id;
+                $user = User::where('username', $username)->first();
+                if (!$user) {
+                    $user = new User;
+                    $user->name = $userArray['name'];
+                    $user->username = $username;
+                    $user->password = $username;
+
+                    $agn = agency::where('agn_id', $userArray['agency']['agn_id'])->first();
+                    if ($agn) {
+                        $user->agency = $agn->id;
+                    } else {
+                        $agn = agency::create(['name' => $userArray['agency']['name'], 'agn_id' => $userArray['agency']['agn_id']]);
+                        $user->agency = $agn->id;
+                    }
+                    $brn = branch::where('brn_id', $userArray['branch']['brn_id'])->first();
+                    if ($brn) {
+                        $user->brn = $brn->id;
+                    } else {
+                        $brn = new branch;
+                        $brn->name = $userArray['branch']['name'];
+                        $brn->brn_id = $userArray['branch']['brn_id'];
+                        $brn->agency = $agn->id;
+                        $brn->save();
+                        $user->brn = $brn->id;
+                    }
+
+                    $user->dpm = '';
+
+                    $user->role = $userArray['role'];
+                    if (!Role::where('name', $userArray['role'])->exists()) {
+                        Role::create(['name' => $userArray['role']]);
+                    }
+                    if (!$user->hasRole($userArray['role'])) {
+                        $user->assignRole($userArray['role']);
+                    }
                 } else {
-                    $agn = agency::create(['name' => $userArray['agency']['name'], 'agn_id' => $userArray['agency']['agn_id']]);
-                    $user->agency = $agn->id;
+                    $agn = agency::where('agn_id', $userArray['agency']['agn_id'])->orWhere('name', $userArray['agency']['name'])->first();
+                    if ($agn) {
+                        $agn->update(['name' => $userArray['agency']['name'], 'agn_id' => $userArray['agency']['agn_id']]);
+                        $user->agency = $agn->id;
+                    } else {
+                        $agn = agency::create(['name' => $userArray['agency']['name'], 'agn_id' => $userArray['agency']['agn_id']]);
+                        $user->agency = $agn->id;
+                    }
+                    $brn = branch::where('brn_id', $userArray['branch']['brn_id'])->orWhere('name', $userArray['branch']['name'])->first();
+                    if ($brn) {
+                        $brn->update(['name' => $userArray['branch']['name'], 'brn_id' => $userArray['branch']['brn_id'], 'agency' => $agn->id]);
+                        $user->brn = $brn->id;
+                    } else {
+                        $brn = new branch;
+                        $brn->name = $userArray['branch']['name'];
+                        $brn->brn_id = $userArray['branch']['brn_id'];
+                        $brn->agency = $agn->id;
+                        $brn->save();
+                        $user->brn = $brn->id;
+                    }
+
                 }
-                $brn = branch::where('brn_id', $userArray['branch']['brn_id'])->orWhere('name', $userArray['branch']['name'])->first();
-                if ($brn) {
-                    $brn->update(['name' => $userArray['branch']['name'], 'brn_id' => $userArray['branch']['brn_id'], 'agency' => $agn->id]);
-                    $user->brn = $brn->id;
-                } else {
-                    $brn = new branch;
-                    $brn->name = $userArray['branch']['name'];
-                    $brn->brn_id = $userArray['branch']['brn_id'];
-                    $brn->agency = $agn->id;
-                    $brn->save();
-                    $user->brn = $brn->id;
+
+                $course_g = course_group::where('code', $userArray['courses'][0]['course_type'])->first();
+                if (!$course_g) {
+                    $course_g = course_group::create([
+                        "name" => $userArray['courses'][0]['name'],
+                        "agn" => $user->agency,
+                        "by" => "api:sso",
+                        "code" => $userArray['courses'][0]['course_type']
+                    ]);
                 }
 
+                $user->save();
+
+                $user_group = user_has_group::where('user_id', $user->id)->where('group_id', $course_g->id)->first();
+                if (!$user_group) {
+                    user_has_group::create([
+                        'user_id' => $user->id,
+                        'group_id' => $course_g->id
+                    ]);
+                }
+
+                $user_group = user_has_group::where('user_id', $user->id)->whereNot('group_id', $course_g->id)->delete();
             }
-
-            $course_g = course_group::where('code', $userArray['courses'][0]['course_type'])->first();
-            if (!$course_g) {
-                $course_g = course_group::create([
-                    "name" => $userArray['courses'][0]['name'],
-                    "agn" => $user->agency,
-                    "by" => "api:sso",
-                    "code" => $userArray['courses'][0]['course_type']
-                ]);
-            }
-
-            $user->save();
-
-            $user_group = user_has_group::where('user_id', $user->id)->where('group_id', $course_g->id)->first();
-            if (!$user_group) {
-                user_has_group::create([
-                    'user_id' => $user->id,
-                    'group_id' => $course_g->id
-                ]);
-            }
-
-            $user_group = user_has_group::where('user_id', $user->id)->whereNot('group_id', $course_g->id)->delete();
 
             Auth::login($user);
             return redirect(route('main'));
         } catch (\Throwable $th) {
             // show error
-            return redirect('/')->with('error', 'Failed to connect to SSO Server!' . $th->getMessage());
+            // return redirect('/')->with('error', 'Failed to connect to SSO Server!');
+            return redirect(config('auth.sso_host') . '?login_sso_error=' . urlencode('Failed to connect to SSO Server!'));
         }
     }
 }
