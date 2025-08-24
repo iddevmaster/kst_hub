c26806b05ce5400d2ca72e8b116c20e767d7f3ed
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-08-24 13:30:01 +0700

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\branch;
 use App\Models\course;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class APIController extends Controller
 {
@@ -14,6 +17,40 @@ class APIController extends Controller
     }
 
     public function fromTsmc(Request $request, $courseId) {
-        dd($courseId, $request->all());
+
+        $request->validate([
+            'name' => 'required|string|max:500',
+            'username' => 'required|string|max:500',
+            'org' => 'required|string|max:500',
+            // Add other fields as necessary
+        ]);
+        if (User::where('username', $request->username)->exists()) {
+            $user = User::where('username', $request->username)->first();
+        } else {
+            $tsmcadmin = User::where('username', 'tsmcschool')->first();
+
+            if (branch::where('name', $request->org)->exists()) {
+                $branch = branch::where('name', $request->org)->first();
+            } else {
+                $branch = branch::create([
+                    'name' => $request->org,
+                    'agency' => $tsmcadmin->agency,
+                ]);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'password' => $request->username,
+                'agency' => $tsmcadmin->agency,
+                'brn' => $branch->id,
+                'dpm' => '',
+                'role' => 'employee',
+                'courses' => [],
+                'startlt' => null,
+            ]);
+        }
+        Auth::login($user);
+        return redirect()->route('course.detail', ['id' => $courseId]);
     }
 }
