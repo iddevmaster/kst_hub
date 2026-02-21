2f923a157c2c1f7b669079464d01c00efd7580e6
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-02-21 10:29:42 +0700

    Update GenTest.php

--- app/Console/Commands/GenTest.php
+++ app/Console/Commands/GenTest.php
@@ -29,62 +29,56 @@ class GenTest extends Command
      */
     public function handle()
     {
+        $this->info('----------  Start generating tests...  ----------');
         $sample_test = Test::whereIn('id', [20907, 20905])->get();
-        $this->info('Sample tests retrieved: ' . count($sample_test));
-
         $sample_answer1 = $sample_test[0]->answers;
-
         $sample_answer2 = $sample_test[1]->answers;
-        $this->info('sample ex: ' . json_encode($sample_answer2) . " \n");
-
         $merged = $sample_answer1 + $sample_answer2;
-
-        // สุ่มเฉพาะ key
-        $randomKeys = Arr::random(array_keys($merged), 30);
-
-        // rebuild array โดยรักษา key เดิม
-        $random = collect($randomKeys)
-            ->mapWithKeys(function ($key) use ($merged) {
-                return [$key => $merged[$key]];
-            })
-            ->toArray();
-
-        // นับ status = 1
-        $count_status = collect($random)
-            ->where('status', '1')
-            ->count();
-
-        $this->info('Random sample: ' . json_encode($random));
-        $this->info('Count of status 1: ' . $count_status);
-        $this->info('total: ' . count($random));
 
         $randomDateTimes = $this->randomDateTimes();
 
-        $users = User::whereBetween('username', ['CP2569041', 'CP2569070'])->whereNot('username', 'CP2569046')->get('id');
+        $users = User::whereBetween('username', ['CP2569041', 'CP2569070'])->whereNot('username', 'CP2569046')->get(['id', 'username']);
         foreach ($users as $index => $user) {
             $startDate = $randomDateTimes[$index % count($randomDateTimes)];
             $endDate = Carbon::parse($startDate)->addSeconds(rand(600, 1800))->format('Y-m-d H:i:s');
-            $this->info('startDate: ' . $startDate . '-- endDate: ' . $endDate);
 
-            // $new_test = new Test();
-            // $new_test->quiz = $sample_test[0]->quiz;
-            // $new_test->course_id = $sample_test[0]->course_id;
-            // $new_test->tester = $user->id;
-            // $new_test->answers = $random;
-            // $new_test->score = $count_status;
-            // $new_test->totalScore = count($random);
-            // $new_test->agn = $sample_test[0]->agn;
-            // $new_test->start = $startDate;
-            // $new_test->end = $endDate;
-            // $new_test->save();
-            // $this->info('Test created for user: ' . $user->username);
+            // สุ่มเฉพาะ key
+            $randomKeys = Arr::random(array_keys($merged), 30);
+
+            // rebuild array โดยรักษา key เดิม
+            $random = collect($randomKeys)
+                ->mapWithKeys(function ($key) use ($merged) {
+                    return [$key => $merged[$key]];
+                })
+                ->toArray();
+
+            // นับ status = 1
+            $count_status = collect($random)
+                ->where('status', '1')
+                ->count();
+
+            $new_test = new Test();
+            $new_test->quiz = $sample_test[0]->quiz;
+            $new_test->course_id = $sample_test[0]->course_id;
+            $new_test->tester = $user->id;
+            $new_test->answers = $random;
+            $new_test->score = $count_status;
+            $new_test->totalScore = count($random);
+            $new_test->agn = $sample_test[0]->agn;
+            $new_test->start = $startDate;
+            $new_test->end = $endDate;
+            $new_test->created_at = $endDate;
+            $new_test->save();
+            $this->info('Test created with ID: ' . $new_test->id . ' for user: ' . $user->username);
         }
+
+        $this->info('----------  End generating tests  ----------');
     }
 
     public function randomDateTimes()
     {
-        $startDate = Carbon::create(2026, 2, 17, 9, 0, 0);
-        $endDate   = Carbon::create(2026, 2, 19, 15, 0, 0);
+        // $startDate = Carbon::create(2026, 2, 17, 9, 0, 0);
+        // $endDate   = Carbon::create(2026, 2, 19, 15, 0, 0);
 
         $results = [];
 
