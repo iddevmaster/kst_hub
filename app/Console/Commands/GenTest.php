956971927428725aceb68ea08ac23b0d2d121462
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-02-21 09:29:31 +0700

    Update GenTest.php

--- app/Console/Commands/GenTest.php
+++ app/Console/Commands/GenTest.php
@@ -29,10 +29,22 @@ class GenTest extends Command
         $sample_test = Test::whereIn('id', [20907, 20905])->get();
         $this->info('Sample tests retrieved: ' . count($sample_test));
 
-        $sample_answer1 = $sample_test[0]->answers;
-        $this->info('Sample answer 1: ' . json_encode($sample_answer1));
+        $sample_answer1 = json_decode($sample_test[0]->answers, true);
 
-        $sample_answer2 = $sample_test[1]->answers;
-        $this->info('Sample answer 2: ' . json_encode($sample_answer2));
+        $sample_answer2 = json_decode($sample_test[1]->answers, true);
+
+        $merged = array_merge($sample_answer1, $sample_answer2);
+
+        $random = collect($merged)
+        ->shuffle()
+        ->take(30);
+
+        $count_status = $random
+        ->where('status', '1')
+        ->count();
+
+        $this->info('Random sample: ' . json_encode($random));
+        $this->info('Count of status 1: ' . $count_status);
+        $this->info('total: ' . count($random));
     }
 }
