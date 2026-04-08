85e058555a72203ab3c743415703a393e6313a32
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2026-04-08 10:47:41 +0700

    Update GenerateUsers.php

--- app/Console/Commands/GenerateUsers.php
+++ app/Console/Commands/GenerateUsers.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use App\Models\User;
+use App\Models\user_has_course;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class GenerateUsers extends Command
 {
@@ -18,7 +20,7 @@ class GenerateUsers extends Command
      * brn_id - branch ID (optional)
      * dpm_id - department ID (optional)
      */
-    protected $signature = 'app:generate-users {prefix} {count} {org_id} {brn_id} {dpm_id} {role?} {gen?}';
+    protected $signature = 'app:generate-users {prefix} {count} {org_id} {brn_id} {dpm_id} {role?} {gen?} {course_id?}';
 
     /**
      * The console command description.
@@ -39,6 +41,7 @@ class GenerateUsers extends Command
         $brnId = $this->argument('brn_id');
         $dpmId = $this->argument('dpm_id');
         $role = $this->argument('role');
+        $course_id = $this->argument('course_id');
 
         if ( User::where('username', 'LIKE', "{$prefix}%")->exists() ) {
              $this->error("Users with the prefix '{$prefix}' already exist. Aborting to prevent duplicates.");
@@ -52,9 +55,10 @@ class GenerateUsers extends Command
                 " | Department ID: {$dpmId}". " ----------"
         );
         for ($i = 1; $i <= $count; $i++) {
-            $username = "{$prefix}-0{$i}";
+            $username = "{$prefix}0{$i}2569";
             $name = "นักเรียนรุ่นที่{$gen} หมายเลข{$i}";
             try {
+                DB::beginTransaction();
                 $user = User::create([
                     'name' => $name,
                     'username' => $username,
@@ -67,11 +71,21 @@ class GenerateUsers extends Command
 
                 $user->assignRole($role ?? 'employee');
 
+                if (!user_has_course::where('user_id', $user->id)->where('course_id', $course_id)->exists()) {
+                    user_has_course::create([
+                        'user_id' => $user->id,
+                        'course_id' => $course_id
+                    ]);
+                }
+
+                DB::commit();
+
                 $this->info(
                     "Generated user: {$username}" .
                     " | Name: นักเรียนรุ่นที่{$gen} หมายเลข{$i}"
                 );
             } catch (\Exception $e) {
+                DB::rollBack();
                 $this->error("Failed to create user {$username}: " . $e->getMessage());
                 continue;
             }
